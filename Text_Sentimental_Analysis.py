05b956a492e56ebbe7e86bd4e0dd8e3a125317b9
Kalyan Vikkurthi <kalyan.vikkurthi@sjsu.edu>
2023-02-07 19:09:51 -0800

    Added additional corpora and lexicons which will accept hindi and spanish  text as well

--- Text_Sentimental_Analysis.py
+++ Text_Sentimental_Analysis.py
@@ -1,5 +1,14 @@
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#Perform analytics with different languages.
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+nltk.download('indian')
+nltk.download('punkt')
+nltk.download('vader_lexicon')
+
+#Added additional corpora and lexicons which will accept hindi and spanish  text as well
+
 def sentiment_analysis(text):
     nltk.download("vader_lexicon")
     sentiment = SentimentIntensityAnalyzer().polarity_scores(text)
